f8cba0211458fdec3203f9eba106fff843ee16fa
Yunnie Yunick <yunnieyunick@gmail.com>
2023-02-22 14:22:15 +0900

    fix fetch

--- pages/api/fetch.ts
+++ pages/api/fetch.ts
@@ -1,5 +1,5 @@
 export const getAllArticles = async () => {
-  const res = await fetch(new URL(`${process.env.API_URL}//v1/articles`))
+  const res = await fetch(new URL(`${process.env.API_URL}/v1/articles`))
   const articles = await res.json()
   return articles
 }

1af2f5e729b2097cf329223b961ef239590a5036
Yunnie Yunick <yunnieyunick@gmail.com>
2023-02-22 12:09:50 +0900

    add env

--- pages/api/fetch.ts
+++ pages/api/fetch.ts
@@ -1,12 +1,12 @@
 export const getAllArticles = async () => {
-  const res = await fetch(new URL('http://localhost:8080/api/v1/articles'))
+  const res = await fetch(new URL(`${process.env.API_URL}//v1/articles`))
   const articles = await res.json()
   return articles
 }
 
 export const getArticle = async (id: string) => {
   const res = await fetch(
-    new URL(`http://localhost:8080/api/v1/articles/${id}`)
+    new URL(`${process.env.API_URL}/v1/articles/${id}`)
   )
   const article = await res.json()
   return article
